e5bd5bc1c2c1bb1caa431071b8ff12f6b7566d21
adi forkasiewicz <adiforka@gmail.com>
2020-05-11 20:23:21 +0200

    committing ex

---
+++ src/factorial_business/BigIntegerFactorial.java
@@ -0,0 +1,44 @@
+package factorial_business;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+//calculate factorial of any integer with BigInteger, iteratively and recursively (tail recursion here)
+public class BigIntegerFactorial {
+
+    //demo
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter an int to get its factorial: ");
+        int input = in.nextInt();
+        System.out.println(calcFactorialIterative(input));
+        System.out.println(calcFactorialRecursive(input));
+
+    }
+
+    public static BigInteger calcFactorialIterative(long num) {
+
+        BigInteger result = BigInteger.ONE;
+        for (int i = 1; i <= num; i++) {
+            result = result.multiply(new BigInteger(i + ""));
+        }
+        return result;
+    }
+
+    public static BigInteger calcFactorialRecursive(long num) {
+        return calcFactorialRecursive(num, BigInteger.ONE);
+    }
+
+    private static BigInteger calcFactorialRecursive(long num, BigInteger result) {
+
+        if (num == 1)
+            return result;
+
+        //num decrements value for next recursive call, bringing the operation closer to base case
+        //the second parameter stores result by multiplying current value of num by the result
+        // of all past recursive calls
+        return calcFactorialRecursive(num--,
+                new BigInteger(num + "").multiply(new BigInteger(result + "")));
+    }
+}
